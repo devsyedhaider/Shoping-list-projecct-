0c45dcb9219289f41321e0421ba4aa5562a86adf
devsyedhaider <devalihaider86@gmail.com>
2026-03-04 15:29:00 +0500

    basic structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
+import img1 from "./assets/1.jpg";
+
+const ShopingItems = [
+  {
+    id: 118836,
+    name: "Product 1",
+    image: img1,
+    balance: 900,
+  },
+  {
+    id: 933372,
+    name: "Product 2",
+    image: img1,
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Product 3",
+    image: img1,
+    balance: 30,
+  },
+];
+
 export default function App() {
   return (
     <div>
-      <h1>hello</h1>
+      <div className="nav">
+        <h1>ShopingCard</h1>
+        <p>
+          🔔
+          <span>0</span>
+        </p>
+      </div>
+      <ShopingCard />
+    </div>
+  );
+}
+
+function ShopingCard() {
+  return (
+    <div className="shoping">
+      {ShopingItems.map((item) => (
+        <Product item={item} key={item.id} />
+      ))}
+    </div>
+  );
+}
+
+function Product({ item }) {
+  return (
+    <div>
+      <img src={item.image} alt={item.name} />
+      <h1>{item.name}</h1>
+      <p>{item.balance}</p>
+      <button>Buy now</button>
     </div>
   );
 }
